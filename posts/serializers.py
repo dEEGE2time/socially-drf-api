b156ae1f2241ac6b6a6a6ca4911afe7275d08a42
dEEGE2time <johannesweidensjo@hotmail.com>
2023-11-15 15:35:46 +0000

    add PostList view and image validation in serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,6 +7,21 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size cannot be larger than 2MB.'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width cannot be larger than 4096px.'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height cannot be larger than 4096px.'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
