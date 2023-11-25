a38417680f685aa55bf83c3a16a9cbb3f4c6f7fb
dEEGE2time <johannesweidensjo@hotmail.com>
2023-11-25 14:35:59 +0000

    start comments app and add model, serializer and view

---
+++ comments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class Comment(models.Model):
+    """
+    The comment model for users to comment
+    under posts.
+    Comment by users
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        return self.content
