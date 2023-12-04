497d817fe878baeedc017151e8f689f59c32bfa6
dEEGE2time <johannesweidensjo@hotmail.com>
2023-12-04 17:25:32 +0000

    add proxy to package.json and comment out axiosDefaults baseURL

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://socially-api-6488718a0b5b.herokuapp.com/";
+// axios.defaults.baseURL = "https://socially-api-6488718a0b5b.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
