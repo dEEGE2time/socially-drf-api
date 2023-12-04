8d303783092615b8f6558264941d4bd20386dd5e
dEEGE2time <johannesweidensjo@hotmail.com>
2023-12-04 18:04:47 +0000

    set up WhiteNoise for static files, add route to serve index template

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://socially-api-6488718a0b5b.herokuapp.com/";
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
